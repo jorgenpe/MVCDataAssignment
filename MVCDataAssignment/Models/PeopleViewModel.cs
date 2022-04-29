20bfe173fe1e2b8a98d85c7c5b098f91196a5179
jorgenpe <82228083+jorgenpe@users.noreply.github.com>
2022-04-29 13:04:23 +0200

    update4

--- MVCDataAssignment/Models/PeopleViewModel.cs
+++ MVCDataAssignment/Models/PeopleViewModel.cs
@@ -1,6 +1,22 @@
-﻿namespace MVCDataAssignment.Models
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MVCDataAssignment.Models
 {
     public class PeopleViewModel
     {
+        public List<Person> PeopleListView { get; set; }
+
+        public string FilterString { get; set; }
+
+        public PeopleViewModel()
+        { 
+            PeopleListView = new List<Person>(); 
+        }
     }
 }
+
